9c093e34ded14f5d3f290abf1f2b8752874621d7
Deven Shah <devenshah2018@gmail.com>
2023-10-15 16:40:10 -0700

    Database connection. REST API Get method (base).

---
+++ Controllers/DatabaseManagementController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+using MongoDB.Bson;
+using System.Collections.Generic;
+using System.Linq;
+using Trekkers_AA.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace Trekkers_AA.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DatabaseManagementController : Controller
+    {
+        private readonly IMongoDatabase _database;
+        public DatabaseManagementController()
+        {
+            var client = new MongoClient("mongodb+srv://trekkers:trekkers@serviceaccess.lovdwri.mongodb.net/?retryWrites=true&w=majority");
+            _database = client.GetDatabase("UserAccess");
+        }
+        
+        [HttpGet]
+        public ActionResult<IEnumerable<UserModel>> GetUser(string email)
+        {
+            var collection = _database.GetCollection<UserModel>("UserCredentials");
+            var model = collection.Find(user => user.email == email).FirstOrDefault();
+
+            if (model == null) {
+                return NotFound();
+            }
+
+            return Ok(model);
+        }
+
+    }
+}
---
+++ Models/UserModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Trekkers_AA.Models;
+
+public class UserModel
+{
+    [BsonId]
+    public ObjectId Id { get; set; }
+    public string? firstName { get; set; }
+    public string? lastName { get; set; }
+    public string? email { get; set; }
+    public string? password { get; set; }
+    public string? birthDate { get; set; }
+}
+
