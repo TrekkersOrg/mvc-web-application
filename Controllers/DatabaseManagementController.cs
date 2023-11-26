ef1826e35bf0bf0068f8ae03adbb6ee12b9dd6c4
Deven Shah <devenshah2018@gmail.com>
2023-11-25 19:37:58 -0800

    logger

--- Controllers/DatabaseManagementController.cs
+++ Controllers/DatabaseManagementController.cs
@@ -80,24 +80,6 @@ namespace Trekkers_AA.Controllers
             }
         }
 
-        [HttpGet]
-        public ActionResult<IEnumerable<SessionModel>> GetUserSession(ObjectId id)
-        {
-            // Connect to database
-            IMongoDatabase userAccessDatabase = _client.GetDatabase("UserAccess");
-
-            // Get the MongoDB collection and find a user by their email.
-            var collection = userAccessDatabase.GetCollection<SessionModel>("UserSession");
-            var model = collection.Find(userSession => userSession.Id == id).FirstOrDefault();
-
-            // If the user is not found, return a 404 Not Found response; otherwise, return the user data.
-            if (model == null)
-            {
-                return NotFound();
-            }
-            return Ok(model);
-        }
-
         [Route("session")]
         [HttpGet]
         public ActionResult<IEnumerable<SessionModel>> GetUserSession(ObjectId id)
