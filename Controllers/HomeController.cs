fcb0976344edc8a3cfdffda3b48019937fc6167c
Apoorva Sharma <sharmaapoorva124@gmail.com>
2024-01-02 15:19:03 -0800

    Created & Linked #AboutUs

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace StriveAI.Controllers
             return View();
         }
 
+// Rendering TheTeam View
+        public IActionResult TheTeam()
+        {
+            return View();
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
