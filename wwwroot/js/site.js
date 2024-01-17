4b6adfaa04316d1dfc63ea9fe9eea659590d66be
Apoorva Sharma <sharmaapoorva124@gmail.com>
2024-01-16 18:21:49 -0800

    DA-92: Added highlighting to navigation buttons

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,19 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$(document).ready(function() {
+    // Add active class to nav-link based on current URL
+    var pathname = window.location.pathname;  // Get current page path
+    $('.navbar-nav .nav-link').each(function() {
+        var href = $(this).attr('href');
+        if (href === pathname) {
+            $(this).addClass('active');
+        }
+    });
+
+    // Handle clicks on nav-links
+    $('.navbar-nav .nav-link').click(function() {
+        $('.navbar-nav .nav-link').removeClass('active');
+        $(this).addClass('active');
+    });
+});
