79a11de5aa9fc49cff7a00eef417398f15bda6d9
Apoorva Sharma <sharmaapoorva124@gmail.com>
2024-01-20 11:28:55 -0800

    DA-49: File Upload static page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -39,6 +39,10 @@ namespace StriveAI.Controllers
         {
             return View();
         }
+        public IActionResult FileUpload()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
