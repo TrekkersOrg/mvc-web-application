d38ac3b0f62e1c486335743e47866c788e048857
Deven Shah <devenshah2018@gmail.com>
2024-02-16 13:20:52 -0800

    DA-156 - Completed /upload endpoint for file upload API

---
+++ Controllers/FileUploadController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StriveAI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FileUploadController : Controller
+    {
+        public FileUploadController() { }
+
+        /// <summary>
+        /// Uploads file to "UserFiles".
+        /// </summary>
+        /// <param name="targetFile" type="IFormFile"></param>
+        /// <returns type="ActionResult"></returns>
+        [HttpPost("upload")]
+        public ActionResult Upload(IFormFile targetFile)
+        {
+            if (targetFile != null && targetFile.Length > 0)
+            {
+                var filePath = Path.Combine(Directory.GetCurrentDirectory(), "UserFiles", targetFile.FileName);
+                using (var fileStream = new FileStream(filePath, FileMode.Create))
+                {
+                    targetFile.CopyTo(fileStream);
+                }
+                return RedirectToAction("FileUpload");
+            }
+            return RedirectToAction("FileUpload");
+        }
+    }
+}
