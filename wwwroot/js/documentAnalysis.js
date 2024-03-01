a184b866c6e0bde90fb17ccaa200ede01da9e561
Deven Shah <devenshah2018@gmail.com>
2024-03-01 15:29:06 -0800

    Consumption of /chatbot/sendQuery on client-side for summary generation.

--- wwwroot/js/documentAnalysis.js
+++ wwwroot/js/documentAnalysis.js
@@ -3,7 +3,7 @@ function generateSummary()
 {
     const summaryParagraph = document.querySelector(".insert-summary p");
     const requestBody = {
-        Namespace: "TestSuite",
+        Vectorstore: "TestSuite",
         Query: "Generate a brief yet informative summary about this especially the critical details (e.g., dates, people, references). Make sure your summary does not exceed 250 words."
     };
     showLoader();
@@ -22,19 +22,12 @@ function generateSummary()
                 displayError("System is under maintenance. Please try again later.")
                 throw new Error(`HTTP error! Status: ${response.status}`);
             }
-            try 
-            {
-                var responseJson = response.json();
-                var responseSummary = responseJson["data"]["response"];
-                localStorage.setItem("documentSummary", responseSummary);
-            } catch (error)
-            {
-                console.error("Error loading response: ", error);
-                displayError("Failed summary generation.");
-            }
-            summaryParagraph.textContent = response.data;
+            return response.json();
         })
-        .then(data => {console.log(data)})
+        .then(data =>
+        {
+            localStorage.setItem("documentSummary",data["data"]["response"]);
+        })
         .catch(error =>
         {
             hideLoader();
