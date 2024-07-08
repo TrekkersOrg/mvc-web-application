e38a804685927e890280c0f33a9067c0c53d84af
devenshah2018 <46404643+devenshah2018@users.noreply.github.com>
2024-07-08 12:31:52 -0700

    DA-368 - Generates a brief summary of the uploaded document in a system bubble, along with additional verbiage.

--- wwwroot/js/documentDashboard.js
+++ wwwroot/js/documentDashboard.js
@@ -507,8 +507,8 @@ async function generateSummary()
         var data = await response.json();
         if (response.ok)
         {
-            var summary = data["data"]["response"];
-            // Inject summary into UI here
+            var summaryResponse = data["data"]["response"] + " Feel free to ask further questions regarding this document.";
+            generateSystemBubble(summaryResponse);
         } else
         {
             displayError("ERROR GENERATING SUMMARY");
@@ -546,7 +546,7 @@ window.onload = async function ()
     showLoader();
     sessionStorage.setItem("conversationMemory",JSON.stringify([]));
     document.getElementById('queryInput').addEventListener('input',checkInput);
-    generateSystemBubble("I am a helpful assistant! Just ask me anything about this document.");
+    //generateSystemBubble("I am a helpful assistant! Just ask me anything about this document.");
     checkInput();
 
     if (JSON.parse(sessionStorage.getItem('documentContext'))?.documentName != "")
