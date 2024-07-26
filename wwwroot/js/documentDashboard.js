1830a3116ffe54d6d5f19c5515db4ba6bec9a8c8
devenshah2018 <46404643+devenshah2018@users.noreply.github.com>
2024-07-25 21:16:40 -0700

    End-to-end functionality

--- wwwroot/js/documentDashboard.js
+++ wwwroot/js/documentDashboard.js
@@ -461,7 +461,11 @@ function conversationMemoryEntry()
             'Access-Control-Allow-Methods': '*',
             'Access-Control-Allow-Headers': '*'
         },
-        body: JSON.stringify(requestBody)
+        body: {
+            namespace: sessionNamespace,
+            query: query,
+            file_name: sessionStorage.getItem('selectedFile')
+        }
     })
         .then(response =>
         {
@@ -524,7 +528,7 @@ async function determineRiskScore()
     //await systemQuery();
     var custom = JSON.parse(sessionStorage.getItem('custom')).data;
     var keywords = JSON.parse(sessionStorage.getItem('keywords')).data;
-    var query = JSON.parse(sessionStorage.getItem('query')).data;
+    var query = JSON.parse(sessionStorage.getItem('keywords')).data;
     var financialScoreAvg = Math.round((custom.financialScore + keywords.financialScore + query.financialScore) / 3);
     var operationalScoreAvg = Math.round((custom.operationalScore + keywords.operationalScore + query.operationalScore) / 3);
     var regulatoryScoreAvg = Math.round((custom.regulatoryScore + keywords.regulatoryScore + query.regulatoryScore) / 3);
@@ -547,16 +551,6 @@ window.onload = async function ()
     sessionStorage.setItem("conversationMemory",JSON.stringify([]));
     document.getElementById('queryInput').addEventListener('input',checkInput);
     checkInput();
-
-    if (JSON.parse(sessionStorage.getItem('documentContext'))?.documentName != "")
-    {
-
-        document.getElementById('document-name').innerText = JSON.parse(sessionStorage.getItem('documentContext'))?.documentName;
-    } else
-    {
-        document.getElementById('document-name').innerText = sessionStorage.getItem('selectedFile');
-
-    }
 
     // Chart.js and risk meter initialization
     var ctx = document.getElementById('spiderChart').getContext('2d');
